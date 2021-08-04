67252486ef19934d1110e05b03c23f48baf784d7
xarantolus <xarantolus@protonmail.com>
2021-08-04 08:41:25 +0200

    Improve test script

--- test.sh
+++ test.sh
@@ -2,21 +2,25 @@ set +e
 
 # All commands return an exit status of 1 when run like this
 
-./ruleset_converter
+./ruleset_converter > /dev/null 2>&1
 retVal=$?
 if [ $retVal -ne 1 ]; then
     echo "Error: Expected ruleset_converter exit code to be 1, but got $retVal"
+    exit 1
 fi
 
-./subresource_indexing_tool
+./subresource_indexing_tool > /dev/null 2>&1
 retVal=$?
 if [ $retVal -ne 1 ]; then
     echo "Error: Expected subresource_indexing_tool exit code to be 1, but got $retVal"
+    exit 1
 fi
 
-./subresource_filter_tool
+./subresource_filter_tool > /dev/null 2>&1
 retVal=$?
 if [ $retVal -ne 1 ]; then
     echo "Error: Expected subresource_filter_tool exit code to be 1, but got $retVal"
+    exit 1
 fi
 
+echo "All tools behave as expected."
