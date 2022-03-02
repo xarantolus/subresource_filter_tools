1803dca94b070418e7c52dbb7ffea6e3f9ddf5dd
xarantolus <xarantolus@protonmail.com>
2022-03-02 09:45:51 +0100

    Manually install Windows 10 SDK

--- windows/build_windows.ps1
+++ windows/build_windows.ps1
@@ -17,7 +17,7 @@ Write-Output "::endgroup::"
 
 Write-Output "::Installing Visual Studio Components"
 
-Start-Process -Wait -NoNewWindow -FilePath cmd -ArgumentList "/c", '"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe"', "--add", "Microsoft.VisualStudio.Workload.NativeDesktop", "--add", "Microsoft.VisualStudio.Component.VC.ATLMFC", "--includeRecommended", "--includeOptional", "--passive", "--locale", "en-US"
+Start-Process -Wait -NoNewWindow -FilePath cmd -ArgumentList "/c", '"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe"', "--add", "Microsoft.VisualStudio.Workload.NativeDesktop", "--add", "Microsoft.VisualStudio.Component.Windows10SDK", "--add", "Microsoft.VisualStudio.Component.Windows10SDK.19041", "--add", "Microsoft.VisualStudio.Component.VC.ATLMFC", "--includeRecommended", "--includeOptional", "--passive", "--locale", "en-US"
 
 Write-Output "::group::Install depot_tools"
 
