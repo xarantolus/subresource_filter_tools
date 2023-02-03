729d303451d3f6ba2f9ebee9e312f9b83466a6f1
xarantolus <xarantolus@protonmail.com>
2023-02-03 09:10:36 +0100

    Try to fix build

--- linux/build_linux.sh
+++ linux/build_linux.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e # exit on error
 
 output() {
@@ -24,7 +25,7 @@ output "Checking depot_tools..."
 if [ -d "depot_tools" ]; then
     cd depot_tools
     git reset --hard HEAD # in case anything was saved here
-    git pull origin "$CURRENT_BRANCH" 
+    git pull origin "$CURRENT_BRANCH"
     cd ..
 else
     # Install depot_tools
@@ -49,8 +50,8 @@ output "Even if this next command outputs errors, it might still work. The scrip
 
 # This is the command that is used to initially fetch the source code
 # It does not work if the repo has already been downloaded - which is why whe need the fallback option
-fetch --nohooks --no-history chromium || failed=1 && output "\"fetch\" failing doesn't matter.\nUsing \"gclient sync\" to download updates, repo likely has already been downloaded" 
-if [ $failed -eq 1 ]; then
+fetch --nohooks --no-history chromium || failed=1 && output "\"fetch\" failing doesn't matter.\nUsing \"gclient sync\" to download updates, repo likely has already been downloaded"
+if [[ $failed -eq 1 ]]; then
   # fallback to sync command to get latest changes. If that one doesn't work, then we're out of luck
   # The -D flag removes any unused/unnecessary parts of the repository that are no longer needed
   gclient sync -D || (output "OK, running \"gclient sync\" also failed.\nYou should probably remove both the chromium/ and depot_tools/ directory and start over." && exit 1)
@@ -71,7 +72,7 @@ echo "::group::Install additional build dependencies"
 output "Installing additional build dependencies..."
 
 # Install additional build dependencies
-./build/install-build-deps.sh --no-prompt --no-syms --no-arm --no-nacl || true 
+./build/install-build-deps.sh --no-prompt --no-syms --no-arm --no-nacl || true
 
 echo "::endgroup::"
 
@@ -82,7 +83,7 @@ output "Running preparation steps..."
 # Run hooks
 gclient runhooks
 
-rm -rf out/Default || true 
+rm -rf out/Default || true
 
 echo "::endgroup::"
 
@@ -91,7 +92,7 @@ echo "::group::Generate build files"
 output "Generating build target..."
 
 # Setting up the build: Generate required build files
-gn gen out/Default --args='target_cpu="x64" target_os="linux" enable_nacl=false is_component_build=false is_debug=false blink_symbol_level=0' 
+gn gen out/Default --args='target_cpu="x64" target_os="linux" enable_nacl=false is_component_build=false is_debug=false blink_symbol_level=0'
 
 echo "::endgroup::"
 
@@ -100,7 +101,7 @@ echo "::group::Build subresource_filter_tools"
 output "Starting build..."
 
 # Build subresource_filter_tools, as that's the part we're interested in
-autoninja -C out/Default subresource_filter_tools 
+autoninja -C out/Default subresource_filter_tools
 
 output "Finished build."
 
